c54f1d799128ea82556d169ed683b39ca7fa66d9
vovandrelo <vovandrelo@gmail.com>
2022-10-26 00:44:15 +0300

    Change store configuration

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,21 +1,27 @@
+//<============================================ ИМПОРТ НЕОБХОДИМЫХ БИБЛИОТЕК ============================================>\\
+
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import { authReducer } from './modules/auth';
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
+//<=========================================== ФОРМИРОВАНИЕ НЕОБХОДИМЫХ ТИПОВ ===========================================>\\
+
+export type AppDispatch = typeof store.dispatch;            // Тип dispatch-а
+export type RootState = ReturnType<typeof store.getState>;  // Тип ГС
+export type AppThunk<ReturnType = void> = ThunkAction<      // Тип Thunk-и
     ReturnType,
     RootState,
     unknown,
     Action<string>
 >;
 
+//<=========================================== СОЗДАНИЕ ГЛОБАЛЬНОГО СОСТОЯНИЯ ===========================================>\\
 const rootReducer = {
     auth: authReducer,
 }
 
+//<================================================ КОНФИГУРАЦИЯ STOR-А =================================================>\\
 export const store = configureStore({
-    reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
-    devTools: true,
+    reducer: rootReducer,                                           // Подключение ГС
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),   // Подключение thun-ок
+    devTools: true,                                                 // Подключение devtools-ов
 });
