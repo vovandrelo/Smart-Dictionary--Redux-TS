4c98cb00c3ffe4aed9dc51ffca9930c9e12d3284
vovandrelo <vovandrelo@gmail.com>
2022-10-27 23:53:08 +0300

    Create validator

--- backend/middlewares/auth-middleware.js
+++ backend/middlewares/auth-middleware.js
@@ -1,8 +1,9 @@
 import Jwt from "jsonwebtoken";
 import { KEY } from "../config.js";
 
+
 class AuthMiddleware {
-    checkJWT(res, req, next) {
+    checkJWT(req, res, next) {
         const token = req.header.authorization.split(" ")[1];
         console.log(Jwt.verify("dsdsds", KEY));
         if (token && Jwt.verify(token, KEY)) {
@@ -14,14 +15,28 @@ class AuthMiddleware {
             res.status(403).json({error: true, message: "Authorization error"})
         }
     }
-    loginValidator(res, req, next) {
+    loginValidator(req, res, next) {
+        console.log("Валидация логина запущена!");
+        console.log(req.body);
+        const { login } = req.body;
+
+        // ^A - если начало строки начинается с А, то true
+        // [^] - если 
+        // A$ - если конец строки А, то true
+        //
+        //
+        //
+
+        const reg = /^[\w\d].+[\w\d]$/;                      // Создали регулярное выражение
+        const test = reg.test(login);           // Используем метод test
+        return res.status(403).json({error: true, isValid: test})
         
     }
-    emailValidator(res, req, next) {
-        
+    emailValidator(req, res, next) {
+        return res.status(403).json({error: true, message: "test"})
     }
-    passValidator(res, req, next) {
-        
+    passValidator(req, res, next) {
+        return res.status(403).json({error: true, message: "test"})
     }
 
 }
--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -1,10 +1,16 @@
 import authController from '../controllers/auth-controller.js';
-
+import authMiddleware from '../middlewares/auth-middleware.js';
 
 import express from 'express';
+
 const authRouter = express.Router();
 
-authRouter.post("/login", authController.login);
+
+
+
+const checkLogin = authMiddleware.loginValidator;
+const checkPass = authMiddleware.passValidator;
+authRouter.post("/login", checkLogin, checkPass, authController.login);
 authRouter.post("/registration", authController.registration);
 
 export default authRouter;
